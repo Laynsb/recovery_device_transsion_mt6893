30b5cfb05afa63b8f74c88a22a3d6d84e294be98
Laynsb <133554507+Laynsb@users.noreply.github.com>
2024-07-05 11:50:13 +0700

    Remove patch touch
    Signed-off-by: Laynsb <133554507+Laynsb@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -56,7 +56,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
-
-# Copy patched touch screen driver module
-PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/touch/tran_touch_patched.ko:recovery/root/lib/modules/tran_touch.ko
